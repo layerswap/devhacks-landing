5d44ce6c8abca7b1a017b726595a8ebd1fa823aa
Aren <aren@bransfer.io>
2023-08-17 17:16:17 +0400

    text color changes, delete unused compononents

--- src/components/pageComponents/CTASection.tsx
+++ src/components/pageComponents/CTASection.tsx
@@ -8,7 +8,12 @@ export default function CTASection() {
                 <h2 className="text-3xl font-bold text-white sm:text-4xl lg:text-5xl">
                     Դևը սպասում է քեզ...
                 </h2>
-                <div className="mt-6 flex items-center justify-center gap-x-6">
+                <p className="mt-4 text-darkblue-100">
+                    Գրանցումը, բաց է մինչև
+                    <br />
+                    Սեպտեմբերի 20
+                </p>
+                <div className="mt-5 flex items-center justify-center gap-x-6">
                     <Link
                         href='https://layerswap.typeform.com/devhacks'
                         className="rounded-full w-fit mx-auto bg-primary px-6 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-primary-600 active:bg-primary-700 transition-colors duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -35,7 +35,7 @@ export default function Custom404() {
                     <div className="text-center">
                         <p className="text-base font-semibold text-primary-600">404</p>
                         <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
-                        <p className="mt-6 text-base leading-7 max-w-xs sm:max-w-none mx-auto text-primary-text">Sorry, we couldn’t find the page you’re looking for.</p>
+                        <p className="mt-6 text-base leading-7 max-w-xs sm:max-w-none mx-auto text-darkblue-100">Sorry, we couldn’t find the page you’re looking for.</p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <Link
                                 href="/"
