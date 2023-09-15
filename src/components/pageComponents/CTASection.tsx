614980a41c2912f274250367e3617c0cbb466b6c
Aren <aren@bransfer.io>
2023-09-15 23:07:19 +0400

    add dino

--- src/components/pageComponents/CTASection.tsx
+++ src/components/pageComponents/CTASection.tsx
@@ -9,15 +9,15 @@ export default function CTASection() {
                     <span className="text-primary">Դևը</span> սպասում է քեզ...
                 </h2>
                 <p className="mt-4 text-darkblue-100">
-                    Գրանցումը բաց է մինչև <span className="text-primary">Սեպտեմբերի 15-ը</span>
+                    Գրանցումը <span className="text-primary">փակ է</span>
                 </p>
                 <div className="mt-5 flex items-center justify-center gap-x-6">
                     <Link
                         id="end"
-                        href='https://layerswap.typeform.com/devhacks'
+                        href='/dino/index.html'
                         className="rounded-full w-fit mx-auto bg-primary px-6 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-primary-600 active:bg-primary-700 transition-colors duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                     >
-                        Գրանցել թիմ
+                        Միևնույն է գրանցել թիմ
                     </Link>
                 </div>
             </div>
