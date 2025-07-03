7eff32352eb0f42d1db873253a91e62ac48cf122
Aren <aren@bransfer.io>
2025-07-03 23:30:50 +0400

    feat(Rules): add participation rules component to the agenda page

--- src/pages/agenda.tsx
+++ src/pages/agenda.tsx
@@ -1,10 +1,12 @@
 import EventAgenda from "@/components/pageComponents/EventAgenda";
 import Layout from "@/components/pageComponents/Layout";
+import Rules from "@/components/pageComponents/Rules";
 
 export default function AgendaPage() {
     return (
         <Layout>
             <EventAgenda />
+            <Rules />
         </Layout>
     )
 }
